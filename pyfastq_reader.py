7386aa9165428c94bcce709808a42c7a4f70fb98
Andy Hauser <andreas.hauser@lmu.de>
2019-01-25 09:05:09 +0100

    Add code

---
+++ pyfastq_reader.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# MIT License see LICENSE
+# -- Andy Hauser <Andreas.Hauser@LMU.de>
+
+from __future__ import print_function
+import sys
+
+def fastq_reader(filename):
+  i = 0
+  with open(filename, 'r') as infile:
+     name = infile.readline().rstrip()
+     while True:
+       i += 1
+       seq = ""
+       for s in infile:
+         if s[0] == '+':
+           commentp = s.rstrip()
+           break
+         else:
+           seq += s.rstrip()
+       qual = ""
+       for q in infile:
+         if len(qual) > 0 and  q[0] == '@':
+           yield name, seq, qual
+           name = q.rstrip()
+           break
+         else:
+           qual += q.rstrip()
+       else:
+         yield name, seq, qual
+         return
+
+if __name__ == '__main__':
+  for filename in sys.argv[1:]:
+    count = 0
+    for head, seq, qual in fastq_reader(filename):
+      count += 1
+    print(count)
+
