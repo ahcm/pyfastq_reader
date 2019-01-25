98d40c3fda8e4fc8753cbb0cb660b55cdd7bce53
Andy Hauser <andreas.hauser@lmu.de>
2019-01-25 11:18:56 +0100

    Switch to setuptools

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
-from distutils.core import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='pyfastq_reader',
-    version='1.0',
+    version='1.0.1',
     author='Andy Hauser',
     author_email='Andreas.Hauser@LMU.de',
-    scripts=['pyfastq_reader.py'],
-    url='https://github.com/ahcm/pyfastq_reader',
     license='LICENSE',
     description='FASTQ file reader',
     long_description='Pure python reader for the FASTQ format used in sequencing',
-    install_requires=[ ],
+    url='https://github.com/ahcm/pyfastq_reader',
+    packages=setuptools.find_packages(),
+    scripts=['pyfastq_reader.py'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
